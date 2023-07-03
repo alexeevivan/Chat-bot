69d46d19f01223c11984e3c2efc6b9f174903ce3
Ivan Alexeev <alexeevivan92@gmail.com>
2023-07-03 22:16:56 +0300

    stats settings added & more info added to txt file

--- cartel-bot.py
+++ cartel-bot.py
@@ -7,7 +7,7 @@ from aiogram.dispatcher.filters import Command
 from aiogram.types import ParseMode
 from aiogram.utils import executor
 
-import config # Импортируем файл с настройками (токен бота)
+import config  # Импортируем файл с настройками (токен бота)
 
 logging.basicConfig(level=logging.INFO)
 
@@ -27,7 +27,11 @@ with open("responses.txt", "r", encoding="utf-8") as file:
         line = line.strip().split(":")
         keyword = line[0].strip().lower()
         response = line[1].strip()
-        responses[keyword] = response.replace(". ", "\n").replace("xxx", "━━━━━━━━━━━━━━━━━━━━\n")
+        responses[keyword] = response.replace("(colon)", ":").replace(". ", "\n").replace("(long line)", "━━━━━━━━━━━━━━━━━━━━\n")
+
+# Словарь для сбора статистики
+statistics = {}
+
 
 # Команда /start
 @dp.message_handler(Command("start"))
@@ -35,11 +39,42 @@ async def cmd_start(message: types.Message):
     # Отправляет приветственное сообщение и инструкцию по использованию бота.
     await message.reply("Привет\U0001F44B!\nЯ твой личный помощник в мире напитков.\nОтправь название коктейля, и я расскажу всё о рецептуре его приготовления>")
 
+
+# Обработчик команды /статистика
+@dp.message_handler(Command("статистика_бота_для_меня"))
+async def cmd_statistics(message: types.Message):
+    # Получаем информацию о количестве запросов от каждого пользователя
+    stat_text = "Статистика использования чат-бота:\n"
+    for user_id, count in statistics.items():
+        user = await bot.get_chat(user_id)
+        username = user.username or user.first_name or user_id
+        stat_text += f"Пользователь {username}: {count} запросов\n"
+
+    # Получаем общее количество пользователей
+    user_count = len(statistics)
+    stat_text += f"\nВсего пользователей: {user_count}"
+
+    # Получаем самый частый запрос
+    if statistics:
+        most_common_request = max(statistics, key=statistics.get)
+        stat_text += f"\nСамый частый запрос: {most_common_request}"
+
+    # Отправляем статистику пользователю
+    await message.reply(stat_text)
+
+
 # Обработчик текстовых сообщений
 @dp.message_handler()
 async def echo(message: types.Message):
     # Отправляет ответ на запрос пользователя из заранее подготовленных ответов.
     user_text = message.text.lower()
+
+    # Обновляем статистику для текущего пользователя
+    user_id = message.from_user.id
+    if user_id in statistics:
+        statistics[user_id] += 1
+    else:
+        statistics[user_id] = 1
 
     # Проверяем, есть ли пользовательский текст в списке подготовленных ответов
     if user_text in responses:
@@ -50,6 +85,7 @@ async def echo(message: types.Message):
         # Если нет, отправляем сообщение о том, что ответ не найден
         await bot.send_message(chat_id=message.chat.id, text="Извините, я не могу найти ответ на Ваш запрос.\nПопробуйте в нём что-нибудь поменять.")
 
+
 def check_connection(dp):
     try:
         # Проверка соединения с Telegram API
@@ -57,6 +93,7 @@ def check_connection(dp):
         logging.info("Соединение с Telegram API успешно установлено")
     except Exception as e:
         logging.exception("Ошибка при проверке соединения с Telegram API: %s", e)
+
 
 if __name__ == "__main__":
     # Добавляем LoggingMiddleware для вывода отладочных сообщений
