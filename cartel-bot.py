0b4d38305cc20a660f47258bd62be3e2e11f5f3c
Ivan Alexeev <alexeevivan92@gmail.com>
2024-01-18 04:21:58 +0300

    english version added

--- cartel-bot.py
+++ cartel-bot.py
@@ -1,7 +1,6 @@
 import logging
 import os
 import sqlite3
-import html2text
 from collections import defaultdict
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
@@ -24,64 +23,108 @@ dp = Dispatcher(bot, storage=storage)
 
 # Connect to the database and create the table
 def create_table():
-	with sqlite3.connect("bot.db") as conn:
-		cursor = conn.cursor()
-		cursor.execute("""
-			CREATE TABLE IF NOT EXISTS responses (
-				keyword TEXT PRIMARY KEY,
-				response TEXT NOT NULL,
-				image_path TEXT NOT NULL,
-				recipe TEXT NOT NULL,
-				method TEXT NOT NULL,
-				glassware TEXT NOT NULL,
-				garnish TEXT NOT NULL,
-				note TEXT NOT NULL,
-				country TEXT NOT NULL,
-				history TEXT NOT NULL
-			)
-		""")
+    with sqlite3.connect("bot.db") as conn:
+        cursor = conn.cursor()
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS responses (
+                keyword TEXT PRIMARY KEY,
+                response TEXT NOT NULL,
+                image_path TEXT NOT NULL,
+                recipe TEXT NOT NULL,
+                method TEXT NOT NULL,
+                glassware TEXT NOT NULL,
+                garnish TEXT NOT NULL,
+                note TEXT NOT NULL,
+                country TEXT NOT NULL,
+                history TEXT NOT NULL
+            )
+        """)
+
+def create_eng_table():
+    with sqlite3.connect("bot_eng.db") as conn:
+        cursor = conn.cursor()
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS responses (
+                keyword TEXT PRIMARY KEY,
+                response TEXT NOT NULL,
+                image_path TEXT NOT NULL,
+                recipe TEXT NOT NULL,
+                method TEXT NOT NULL,
+                glassware TEXT NOT NULL,
+                garnish TEXT NOT NULL,
+                note TEXT NOT NULL,
+                country TEXT NOT NULL,
+                history TEXT NOT NULL
+            )
+        """)
 
 # Insert data from the file into the database
 def insert_data_to_db():
-	with sqlite3.connect("bot.db") as conn:
-		cursor = conn.cursor()
+    with sqlite3.connect("bot.db") as conn:
+        cursor = conn.cursor()
 
-		with open("responses.txt", "r", encoding="utf-8") as file:
-			for line in file:
-				line = line.strip().split(":")
-				keyword = line[0].strip().lower()
-				response = line[1].strip().replace("(break)", "\n")
-				image_path = line[2].strip()
-				recipe = line[3].strip().replace("---", "\n- ").replace("(colon)", ":").replace("(break)", "\n")
-				method = line[4].strip().replace("---", "\n-").replace("(colon)", ":").replace("(break)", "\n")
-				glassware = line[5].strip().replace("---", "\n-")
-				garnish = line[6].strip().replace("---", "\n-").replace("(colon)", ":")
-				note = line[7].strip().replace("---", "\n- ").replace("(colon)", ":").replace("(break)", "\n")
-				country = line[8].strip()
-				history = line[9].strip().replace("---", "\n").replace("(colon)", ":").replace("(break)", "\n")
+        with open("responses.txt", "r", encoding="utf-8") as file:
+            for line in file:
+                line = line.strip().split(":")
+                keyword = line[0].strip().lower()
+                response = line[1].strip().replace("(break)", "\n")
+                image_path = line[2].strip()
+                recipe = line[3].strip().replace("---", "\n- ").replace("(colon)", ":").replace("(break)", "\n")
+                method = line[4].strip().replace("---", "\n-").replace("(colon)", ":").replace("(break)", "\n")
+                glassware = line[5].strip().replace("---", "\n-")
+                garnish = line[6].strip().replace("---", "\n-").replace("(colon)", ":")
+                note = line[7].strip().replace("---", "\n- ").replace("(colon)", ":").replace("(break)", "\n")
+                country = line[8].strip()
+                history = line[9].strip().replace("---", "\n").replace("(colon)", ":").replace("(break)", "\n")
 
-				cursor.execute(
-					"INSERT OR REPLACE INTO responses (keyword, response, image_path, recipe, method, glassware, garnish, note, country, history) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-					(keyword, response, image_path, recipe, method, glassware, garnish, note, country, history)
-				)
+                cursor.execute(
+                    "INSERT OR REPLACE INTO responses (keyword, response, image_path, recipe, method, glassware, garnish, note, country, history) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                    (keyword, response, image_path, recipe, method, glassware, garnish, note, country, history)
+                )
+
+# Insert data from the file into the database
+def insert_data_to_eng_db():
+    with sqlite3.connect("bot_eng.db") as conn:
+        cursor = conn.cursor()
+
+        with open("responses_eng.txt", "r", encoding="utf-8") as file:
+            for line in file:
+                line = line.strip().split(":")
+                keyword = line[0].strip().lower()
+                response = line[1].strip().replace("(break)", "\n")
+                image_path = line[2].strip()
+                recipe = line[3].strip().replace("---", "\n- ").replace("(colon)", ":").replace("(break)", "\n")
+                method = line[4].strip().replace("---", "\n-").replace("(colon)", ":").replace("(break)", "\n")
+                glassware = line[5].strip().replace("---", "\n-")
+                garnish = line[6].strip().replace("---", "\n-").replace("(colon)", ":")
+                note = line[7].strip().replace("---", "\n- ").replace("(colon)", ":").replace("(break)", "\n")
+                country = line[8].strip()
+                history = line[9].strip().replace("---", "\n").replace("(colon)", ":").replace("(break)", "\n")
+
+                cursor.execute(
+                    "INSERT OR REPLACE INTO responses (keyword, response, image_path, recipe, method, glassware, garnish, note, country, history) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                    (keyword, response, image_path, recipe, method, glassware, garnish, note, country, history)
+                )
 
 # Create the table and insert data if it's the first run
 create_table()
+create_eng_table()
 insert_data_to_db()
+insert_data_to_eng_db()
 
 # Dictionary to collect statistics
 statistics = defaultdict(int)
-# Dictionary to store user selected language
+# Dictionary to store user-selected language
 user_language = {}
 # Welcome messages in different languages
 welcome_messages = {
-	"ru": "Привет\U0001F44B!\nЯ твой личный помощник в мире смешанных напитков. Я говорю на русском языке.\nОтправь название коктейля, и я расскажу всё о рецептуре его приготовления.",
-	"en": "Hello\U0001F44B!\nI'm your personal assistant in the world of mixed drinks. I speak English.\nSend the name of a cocktail, and I'll tell you all about its recipe."
+    "ru": "Привет\U0001F44B!\nЯ твой личный помощник в мире смешанных напитков. Я говорю на русском языке.\nОтправь название коктейля, и я расскажу всё о рецептуре его приготовления.",
+    "en": "Hello\U0001F44B!\nI'm your personal assistant in the world of mixed drinks. I speak English.\nSend the name of a cocktail, and I'll tell you all about its recipe."
 }
-# Словарь с файлами для разных языков
+# Dictionary with files for different languages
 language_files = {
-    "ru": 'responses.txt',
-    "en": 'responses_eng.txt',
+    "ru": 'bot.db',
+    "en": 'bot_eng.db',
 }
 
 # Command /setlang
@@ -219,28 +262,29 @@ async def cmd_statistics(message: types.Message):
 # Text messages handler
 @dp.message_handler()
 async def echo(message: types.Message):
-	user_text = message.text.lower()
-	user_id = message.from_user.id
-	user_language_code = user_language.get(user_id, "ru")
-	unique_users.add(user_id)
+    user_text = message.text.lower()
+    user_id = message.from_user.id
+    user_language_code = user_language.get(user_id, "ru")
+    filename = language_files.get(user_language_code, "ru")
+    unique_users.add(user_id)
 
-	if user_text in statistics:
-		count, most_common_request = statistics[user_text]
-		count += 1
-		statistics[user_text] = (count, most_common_request)
-	else:
-		statistics[user_text] = (1, user_text)
+    if user_text in statistics:
+        count, most_common_request = statistics[user_text]
+        count += 1
+        statistics[user_text] = (count, most_common_request)
+    else:
+        statistics[user_text] = (1, user_text)
 
-	with sqlite3.connect("bot.db") as conn:
-		cursor = conn.cursor()
+    with sqlite3.connect(filename) as conn:
+        cursor = conn.cursor()
 
-		cursor.execute("SELECT response, image_path, recipe, method, glassware, garnish, note, country, history FROM responses WHERE keyword = ?", (user_text,))
-		data = cursor.fetchone()
+        cursor.execute("SELECT response, image_path, recipe, method, glassware, garnish, note, country, history FROM responses WHERE keyword = ?", (user_text,))
+        data = cursor.fetchone()
 
-		if data:
-			response, image_path, recipe, method, glassware, garnish, note, country, history = data
+        if data:
+            response, image_path, recipe, method, glassware, garnish, note, country, history = data
 
-			full_response = f"""
+            full_response = f"""
 {response}
 
 {recipe}
@@ -259,39 +303,41 @@ async def echo(message: types.Message):
 {history}
 """
 
-			with open(image_path, "rb") as photo:
-				# Отправляем только фотографию без подписи
-				await bot.send_photo(
-					chat_id=message.chat.id,
-					photo=InputFile(photo),
-					caption=None  # Устанавливаем подпись как None, чтобы не было подписи у фотографии
-				)
+            with open(image_path, "rb") as photo:
+                # Отправляем только фотографию без подписи
+                await bot.send_photo(
+                    chat_id=message.chat.id,
+                    photo=InputFile(photo),
+                    caption=None  # Устанавливаем подпись как None, чтобы не было подписи у фотографии
+                )
 
-			# Отправляем текст как отдельное сообщение без подписи
-			await bot.send_message(
-				chat_id=message.chat.id,
-				text=full_response,
-				parse_mode=ParseMode.HTML
-			)
-		else:
-			if user_language_code == "ru":
-				await bot.send_message(
-					chat_id=message.chat.id,
-					text="Извините, я не могу найти ответ на Ваш запрос.\nПопробуйте в нём что-нибудь поменять."
-				)
-			if user_language_code == "en":
-				await bot.send_message(
-					chat_id=message.chat.id,
-					text="Sorry, I can't find an answer to your query.\nTry to change something in it."
-				)
+            # Отправляем текст как отдельное сообщение без подписи
+            await bot.send_message(
+                chat_id=message.chat.id,
+                text=full_response,
+                parse_mode=ParseMode.HTML
+            )
+        else:
+            if user_language_code == "ru":
+                await bot.send_message(
+                    chat_id=message.chat.id,
+                    text="Извините, я не могу найти ответ на Ваш запрос.\nПопробуйте в нём что-нибудь поменять."
+                )
+            if user_language_code == "en":
+                await bot.send_message(
+                    chat_id=message.chat.id,
+                    text="Sorry, I can't find an answer to your query.\nTry to change something in it."
+                )
 # Запускаем проверку соединения с Telegram API перед запуском бота
 def check_connection(dp):
-	try:
-		bot.get_me()
-		logging.info("Соединение с Telegram API успешно установлено")
-	except Exception as e:
-		logging.exception("Ошибка при проверке соединения с Telegram API: %s", e)
+    try:
+        bot.get_me()
+        logging.info("Соединение с Telegram API успешно установлено")
+    except Exception as e:
+        logging.exception("Ошибка при проверке соединения с Telegram API: %s", e)
 
 if __name__ == "__main__":
-	dp.middleware.setup(LoggingMiddleware())
-	executor.start_polling(dp)
+    create_table()  # Создать таблицу, если она не существует
+    insert_data_to_db()  # Заполнить таблицу данными
+    dp.middleware.setup(LoggingMiddleware())
+    executor.start_polling(dp)
